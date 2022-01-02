9479826f2fd138b61415f8a9f2be4760d6f715d3
TWal <theophile@wallez.net>
2022-01-02 15:58:37 +0100

    Add some CSS with Bulma

---
+++ imacs_app/templatetags/form_tags.py
@@ -0,0 +1,31 @@
+from django import forms
+from django import template
+
+register = template.Library()
+
+@register.filter
+def is_select(field):
+    return isinstance(field.field.widget, forms.Select)
+
+@register.filter
+def is_textarea(field):
+    return isinstance(field.field.widget, forms.Textarea)
+
+@register.filter
+def is_input(field):
+    return isinstance(field.field.widget, (
+        forms.TextInput,
+        forms.NumberInput,
+        forms.EmailInput,
+        forms.PasswordInput,
+        forms.URLInput
+    ))
+
+@register.filter
+def with_class(field, template_class):
+    field_classes = field.field.widget.attrs.get('class', '')
+    field_classes += " " + template_class
+    if len(field.errors) > 0:
+        field_classes += ' is-danger'
+    return field.as_widget(attrs={"class": field_classes})
+
