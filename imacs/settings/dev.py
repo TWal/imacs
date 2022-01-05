cde62c50633121bb6214587516e7469a81bbbfe4
TWal <theophile@wallez.net>
2022-01-05 22:43:09 +0100

    Split settings into several files

---
+++ imacs/settings/dev.py
@@ -0,0 +1,3 @@
+from .app_settings import *
+from .dev_server_settings import *
+from .secrets import *
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imacs.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imacs.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
