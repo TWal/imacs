d405b506770a052345744d17e5f3bd40be16f77a
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-01-10 08:00:00 +0100

    nixos deployment

---
+++ dev_secrets.sh
@@ -0,0 +1,1 @@
+export SECRET_KEY='django-insecure-^lk!jvtyu281-st+&&^#hgtmxl1mggajw8p6qkg!ul8m=58d%)'
--- imacs/settings/dev.py
+++ imacs/settings/dev.py
@@ -1,3 +1,22 @@
+from os import environ
+from pathlib import Path
 from .app_settings import *
-from .dev_server_settings import *
-from .secrets import *
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+SECRET_KEY=environ.get('SECRET_KEY')
