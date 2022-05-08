eccfe25173c8077a7fc00a27e29062c4fc59d37f
Luc Chabassier <luc@dwarfmaster.net>
2022-05-08 09:43:04 +0200

    Create proper nixos module (#3)
    * Create proper nixos module
    
    * Update to new django-nixos interface
    
    * Improve security
    
    * Update lock to right version of django-nixos

---
+++ imacs/settings/nix-unsafe.py
@@ -0,0 +1,17 @@
+from os import environ
+from .app_settings import *
+
+SECRET_KEY=environ.get('SECRET_KEY')
+STATIC_ROOT=environ.get('STATIC_ROOT')
+ALLOWED_HOSTS = list(environ.get('ALLOWED_HOSTS', default='').split(','))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DB_NAME'),
+        'HOST': '',
+    }
+}
+DEBUG = False
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
---
+++ module.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.imacs;
+  inherit (lib) types;
+in {
+  options = {
+    services.imacs = {
+      enable = lib.mkEnableOption "Intelligent Multi-Agent Chores Scheduler";
+      unsafeSettings = lib.mkOption {
+        description = ''
+          Disable security settings, for testing only.
+          You may have to clear your browser HSTS cache for it to take effect.
+        '';
+        type = types.bool;
+        default = false;
+      };
+      hostName = lib.mkOption {
+        description = "The hostname IMACS is served on";
+        type = types.nonEmptyStr;
+        default = "localhost";
+      };
+      keysFile = lib.mkOption {
+        description = "Path to the secret keys file";
+        type = types.either types.nonEmptyStr types.path;
+      };
+      setupNginx = lib.mkOption {
+        description = "Whether to setup nginx";
+        type = types.bool;
+        default = true;
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable
+    (lib.mkMerge [
+      {
+        services.wsgi.enable = true;
+        services.wsgi.applications.imacs = {
+          root = ./.;
+          inherit (cfg) keysFile setupNginx hostName;
+          unixSocket.path = "/run/imacs.sock";
+          django.settings =
+            if cfg.unsafeSettings
+            then "imacs.settings.nix-unsafe"
+            else "imacs.settings.nix";
+          security.noNetwork = true;
+        };
+      }
+      (lib.mkIf (cfg.enable && cfg.setupNginx) { services.nginx.enable = true; })
+      (lib.mkIf (cfg.enable && cfg.setupNginx && !cfg.unsafeSettings) {
+        services.nginx.virtualHosts."${cfg.hostName}".forceSSL = true;
+      })
+      (lib.mkIf cfg.unsafeSettings { warnings = [ "IMACS is configured with unsafe settings" ]; })
+    ]);
+}
